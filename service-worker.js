1695800828901a687fd8cb98ed01a3da171ee28c
Arbouz Abderrahmane <abdouarbouz56@gmail.com>
2025-11-18 13:34:22 +0100

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'salary-calculator-v1';
+const urlsToCache = [
+  '/',
+  '/calcuate-salary/',
+  '/calcuate-salary/index.html',
+  '/calcuate-salary/style.css',
+  '/calcuate-salary/script.js'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
